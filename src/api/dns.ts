50f8a7932e8fe9c88d6284aec90c627c91ea56cd
wstar1990 <34098077+wstar1990@users.noreply.github.com>
2025-03-28 12:48:02 +0800

    Update dns.ts
    增加zoneId,单独传递

--- src/api/dns.ts
+++ src/api/dns.ts
@@ -104,14 +104,16 @@ type EditDnsRecordRequest = {
     proxied?: boolean
 }
 
-
-export async function patchRecord(record: CloudflareDnsRecord, editRequest: EditDnsRecordRequest) {
+//增加zoneId,单独传递
+//export async function patchRecord(record: CloudflareDnsRecord, editRequest: EditDnsRecordRequest) {
+export async function patchRecord(zoneId: string,record: CloudflareDnsRecord, editRequest: EditDnsRecordRequest) {
     const axios = useAxios()
     try {
         await axios.request({
             method: 'patch',
             data: editRequest,
-            url: `/zones/${record.zoneId}/dns_records/${record.id}`,
+            //url: `/zones/${record.zoneId}/dns_records/${record.id}`,
+            url: `/zones/${zoneId}/dns_records/${record.id}`,
         })
     } catch (err) {
         return err.response.data.errors[0].message
